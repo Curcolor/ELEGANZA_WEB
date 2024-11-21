7df900ced30523f349249cfb26f96448e18cf370
Curcolor <jairoarcia2003@gmail.com>
2024-11-21 12:51:37 -0500

    configuracion_para servidor

--- config.py
+++ config.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = f"mysql://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}@{os.getenv('MYSQL_HOST')}/{os.getenv('MYSQL_DATABASE')}"
+    SECRET_KEY = os.getenv('SECRET_KEY', 'tu_clave_secreta')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql://root:lunarspace@localhost/eleganza_ecommerce')
+    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith('postgres://'):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace('postgres://', 'postgresql://', 1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
