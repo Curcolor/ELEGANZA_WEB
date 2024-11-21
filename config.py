fe0c56ea827190869f112f6d2b8d0989b9b6a528
Curcolor <jairoarcia2003@gmail.com>
2024-11-21 13:17:13 -0500

    deploy attemp 2

--- config.py
+++ config.py
@@ -6,12 +6,12 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'tu_clave_secreta')
     
-    # Construir DATABASE_URL para MySQL
+    # Construir DATABASE_URL para MySQL con PyMySQL
     DB_USER = os.getenv('MYSQLUSER', 'root')
     DB_PASSWORD = os.getenv('MYSQLPASSWORD', 'lunarspace')
     DB_HOST = os.getenv('MYSQLHOST', 'localhost')
     DB_NAME = os.getenv('MYSQLDATABASE', 'eleganza_ecommerce')
     DB_PORT = os.getenv('MYSQLPORT', '3306')
     
-    SQLALCHEMY_DATABASE_URI = f"mysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
