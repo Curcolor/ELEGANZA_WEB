3d3a171d9a3c3ffdafa23c980b17d1a6b34a3390
Curcolor <jairoarcia2003@gmail.com>
2024-11-21 21:54:16 -0500

    cambios al run.py

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 from flask import Flask
 import os
 
+app = Flask(__name__, instance_relative_config=True)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.getenv('PORT', 8080)))
